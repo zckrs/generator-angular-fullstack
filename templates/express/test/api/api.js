cf7e3fb546849fb36e71f27e79eac7bac6c9d559
Tyler Henkel <tylerhenkel@hotmail.com>
2014-02-22 03:50:55 -0500

    simplified mocha test

--- templates/express/test/api/api.js
+++ templates/express/test/api/api.js
@@ -1,36 +1,20 @@
 'use strict';
 
-/**
- * Module dependencies.
- */
-
 var should = require('should'),
   app = require('../../../server'),
-  supertest = require('supertest'),
-  port = process.env.PORT || 3000,
-  api = supertest('http://localhost:' + port);
+  request = require('supertest');
 
-describe('/api/awesomeThings', function() {
-  it('should return things as JSON', function(done) {
-    api.get('/api/awesomeThings')
-    .expect(200)
-    .expect('Content-Type', /json/)
-    .end(function(err, res) {
-      if (err) return done(err);
-      res.body.should.be.instanceof(Array);
-      done();
-    });
-  });
+describe('GET /api/awesomeThings', function() {
   
-  describe('a thing', function() {
-    it('should have a name', function(done) {
-      api.get('/api/awesomeThings')
+  it('should respond with JSON array', function(done) {
+    request(app)
+      .get('/api/awesomeThings')
+      .expect(200)
+      .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
-        var thing = res.body[0];  
-        thing.name.length.should.not.be.eql(0);
+        res.body.should.be.instanceof(Array);
         done();
       });
-    });
   });
 });
