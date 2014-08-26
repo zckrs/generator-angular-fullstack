9a24f874c08cfff5d0a8c2751f490e14d3804f17
zckrs <mdara@eleven-labs.com>
2014-08-26 15:41:16 +0200

    Wip watch

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -116,6 +116,42 @@ gulp.task('autoprefixer', ['bowerInstall'], function() {
     .pipe(gulp.dest('.tmp/'));
 });
 
+var app;
+gulp.task('express', ['autoprefixer'], function() {
+
+  app = require('child_process').spawn('node', ['--debug', 'server/app.js']);
+  app.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+    var str = data.toString().replace(/(\r?\n)/g, '');
+    console.log('stdout: ' + str);
+  });
+
+  app.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+  });
+
+  app.on('close', function (code, signal) {
+    console.log('child process terminated due to receipt of signal '+signal);
+  });
+
+});
+gulp.task('wait', ['express'], function(callback) {
+  console.log('Wait express server start.');
+  setTimeout(function () {
+    callback();
+  }, 1500);
+});
+gulp.task('open', ['wait'], function(callback) {
+  require('open')('http://localhost:9000');
+  callback();
+});
+gulp.task('quit', ['express'], function(callback) {
+  setTimeout(function () {
+      app.kill();
+      callback();
+  }, 100000);
+});
+
 gulp.task('serveNew',
   [
     'clean:server',
@@ -125,7 +161,11 @@ gulp.task('serveNew',
     'injector:css',
     'injector:scripts',
     'bowerInstall',
-    'autoprefixer'
+    'autoprefixer',
+    'express',
+    'wait',
+    'open',
+    'watch'
   ],
   function () {
 
@@ -134,131 +174,42 @@ gulp.task('serveNew',
 
 
 
-
-
-
-
-
-
-gulp.task('styles', function () {<% if (includeSass) { %>
-  return gulp.src('app/styles/main.scss')
-    .pipe($.plumber())
-    .pipe($.rubySass({
-      style: 'expanded',
-      precision: 10
-    }))<% } else { %>
-  return gulp.src('app/styles/main.css')<% } %>
-    .pipe($.autoprefixer('last 1 version'))
-    .pipe(gulp.dest('.tmp/styles'));
-});
-
-gulp.task('jshint', function () {
-  return gulp.src('app/scripts/**/*.js')
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'))
-    .pipe($.jshint.reporter('fail'));
-});
-
-gulp.task('html', ['styles'], function () {<% if (includeBootstrap && includeSass) { %>
-  var lazypipe = require('lazypipe');
-  var cssChannel = lazypipe()
-    .pipe($.csso)
-    .pipe($.replace, 'bower_components/bootstrap-sass-official/vendor/assets/fonts/bootstrap','fonts');<% } %>
-
-  return gulp.src('app/*.html')
-    .pipe($.useref.assets({searchPath: '{.tmp,app}'}))
-    .pipe($.if('*.js', $.uglify()))<% if (includeBootstrap && includeSass) { %>
-    .pipe($.if('*.css', cssChannel()))<% } else { %>
-    .pipe($.if('*.css', $.csso()))<% } %>
-    .pipe($.useref.restore())
-    .pipe($.useref())
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('images', function () {
-  return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
-    .pipe(gulp.dest('dist/images'));
-});
-
-gulp.task('fonts', function () {
-  var streamqueue = require('streamqueue');
-  return streamqueue({objectMode: true},
-      $.bowerFiles(),
-      gulp.src('app/fonts/**/*')
-    )
-    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
-    .pipe($.flatten())
-    .pipe(gulp.dest('dist/fonts'));
-});
-
-gulp.task('extras', function () {
-  return gulp.src(['app/*.*', '!app/*.html'], {dot: true})
-    .pipe(gulp.dest('dist'));
-});
-
-
-
-gulp.task('connect', function () {
-  var connect = require('connect');
-  var app = connect()
-    .use(require('connect-livereload')({port: 35729}))
-    .use(connect.static('app'))
-    .use(connect.static('.tmp'))
-    // paths to bower_components should be relative to the current file
-    // e.g. in app/index.html you should use ../bower_components
-    .use('/bower_components', connect.static('bower_components'))
-    .use(connect.directory('app'));
-
-  require('http').createServer(app)
-    .listen(9000)
-    .on('listening', function () {
-      console.log('Started connect web server on http://localhost:9000');
-    });
-});
-
-gulp.task('serve', ['connect'<% if (includeSass) { %>, 'styles'<% } %>], function () {
-  require('opn')('http://localhost:9000');
-});
-
-// inject bower components
-gulp.task('wiredep', function () {
-  var wiredep = require('wiredep').stream;
-<% if (includeSass) { %>
-  gulp.src('app/styles/*.scss')
-    .pipe(wiredep({directory: 'bower_components'}))
-    .pipe(gulp.dest('app/styles'));
-<% } %>
-  gulp.src('app/*.html')
-    .pipe(wiredep({
-      directory: 'bower_components'<% if (includeSass && includeBootstrap) { %>,
-      exclude: ['bootstrap-sass-official']<% } %>
-    }))
-    .pipe(gulp.dest('app'));
-});
-
-gulp.task('watch', ['connect', 'serve'], function () {
+gulp.task('watch', ['open'], function () {
   $.livereload.listen();
 
-  // watch for changes
-  gulp.watch([
-    'app/*.html',
-    '.tmp/styles/**/*.css',
-    'app/scripts/**/*.js',
-    'app/images/**/*'
-  ]).on('change', $.livereload.changed);
+  var watcherLess = gulp.watch('./client/{app,components}/**/*.less', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    app.kill();
+    gulp.src('client/app/app.less')
+      .pipe($.less({
+        paths: [
+          'client/bower_components',
+          'client/app',
+          'client/components'
+        ]
+      }))
+      .pipe(gulp.dest('.tmp/app'));
+    app = require('child_process').spawn('node', ['--debug', 'server/app.js']);
+    app.stdout.on('data', function (data) {
+      console.log('stdout: ' + data);
+      var str = data.toString().replace(/(\r?\n)/g, '');
+      console.log('stdout: ' + str);
+    });
 
-  gulp.watch('app/styles/**/*.<%= includeSass ? 'scss' : 'css' %>', ['styles']);
-  gulp.watch('bower.json', ['wiredep']);
-});
+    app.stderr.on('data', function (data) {
+      console.log('stderr: ' + data);
+    });
 
-gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras'], function () {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
-});
+    app.on('close', function (code, signal) {
+      console.log('child process terminated due to receipt of signal '+signal);
+    });
 
-gulp.task('default', ['clean'], function () {
-  gulp.start('build');
+    console.log('Wait express server start.');
+
+  });
+  watcherLess.on('change', function (event) {
+    setTimeout(function () {
+      $.livereload.changed(event.path);
+    }, 250);
+  });
 });
